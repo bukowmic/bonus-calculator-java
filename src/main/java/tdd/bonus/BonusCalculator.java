a88b90588ea221862cc3caa3d8cc8880cb07baa8
bukowmic <bukowmic@gmail.com>
2020-06-15 22:41:43 +0200

    Zadanie 1. Individual bonus - refactor

--- src/main/java/tdd/bonus/BonusCalculator.java
+++ src/main/java/tdd/bonus/BonusCalculator.java
@@ -5,13 +5,21 @@ import java.math.BigDecimal;
 public class BonusCalculator {
 
     public BigDecimal calculateIndividualBonus(Salesman salesman) {
-        int salesRealization = getSalesRealization(salesman.getSales(), salesman.getQuota());
-        double bonusWithoutTax = (salesRealization * salesman.getCommissionPercentage()) / 100.0;
-        double bonusWithTax = (bonusWithoutTax * salesman.getTaxPercentage()) / 100.0;
-        return new BigDecimal(bonusWithTax);
+        int salesRealization = calculateSalesRealization(salesman.getSales(), salesman.getQuota());
+        double bonusCommission = calculateBonusCommission(salesRealization, salesman.getCommissionPercentage());
+        double bonusWithTax = calculateIndividualBonus(bonusCommission, salesman.getTaxPercentage());
+        return BigDecimal.valueOf(bonusWithTax);
     }
 
-    private int getSalesRealization(int sales, int quota) {
+    private int calculateSalesRealization(int sales, int quota) {
         return sales > quota ? sales - quota : 0;
+    }
+
+    private double calculateBonusCommission(int salesRealization, int commissionPercentage) {
+        return (salesRealization * commissionPercentage) / 100.0;
+    }
+
+    private double calculateIndividualBonus(double bonusCommission, int taxPercentage) {
+        return (bonusCommission * taxPercentage) / 100.0;
     }
 }
